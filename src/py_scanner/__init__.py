ea4311a4bca38a422dddd508345e8d106256ead2
PilotLep <pilot1782@pilot1782.org>
2025-03-25 17:10:27 -0600

    llm checkpoint

--- src/py_scanner/__init__.py
+++ src/py_scanner/__init__.py
@@ -1,9 +1,7 @@
-"""
-alksdjdh
-"""
 import json
 import os
 import re
+import ollama
 
 def os_ify_path(path: str) -> str:
     """
@@ -22,21 +20,20 @@ src_dir = os_ify_path(os.path.split(cur_file)[0])
 root_dir = os_ify_path(os.path.split(src_dir)[0])
 print(src_dir)
 hotspots = json.loads(
-    open(
-        os.path.join(
-            src_dir,
-            "example_hotspots.json"
-        ),
-        'r'
-    ).read()
+    open(os.path.join(
+        src_dir,
+        "example_hotspots.json"
+    )).read()
 )["hotspots"]
 
-def parse_spot(spot: dict) -> str:
+def parse_spot(spot: dict) -> tuple[str, str, str]:
     """
     Given a hotspot as a dictionary, parse it and return the relevant code
 
+    If the
+
     :param spot: The hotspot from the list
-    :return: The text of the function containing the hotspot
+    :return: (<parent object>, <lines>, <file>)
     """
 
     file_path = spot["component"].split(":")[1]
@@ -44,46 +41,224 @@ def parse_spot(spot: dict) -> str:
     msg = spot["message"]
     blame = spot["author"]
     rule_key = spot["ruleKey"]
-    with open(file_path, "r") as f:
+    with open(file_path) as f:
         text = f.read()
     print(f"{msg} of type {rule_key} in {file_path} written by {blame}")
+
+
+    lines = text.split("\n")
+    start_line, end_line = curly_context(lines, spot["line"])
+
+    fun_text = "\n".join(lines[start_line:end_line + 1])
+
+    vuln_line = ""
+    i = spot["line"] - 1
+    while not vuln_line.endswith(";") and i < len(lines):
+        vuln_line += " " + lines[i].strip()
+        i += 1
+
+    imports = "\n".join(filter(lambda x: "import" in x, lines))
+
+    return fun_text, vuln_line, imports
+
+def curly_context(text: list[str], start: int) -> tuple[int, int]:
+    """
+    Pulls the parent object/function/class
+
+    :param text: list of text lines
+    :param start: start line
+    :param end: end line
+    :return: (start, end)
+    """
 
     function_regex = re.compile(
         r"\w*\s+"  # return type
         r"\w+\s*"  # function name
         r"\([^)]*\)\s*"  # function parameters
-        r"\w*\s\w*\s"  # throw type
+        r"((throws)\s\w+\s)*"  # throw type
         r"\{",  # start of function
-        re.MULTILINE,
+        re.MULTILINE & re.IGNORECASE,
     )
-    lines = text.split("\n")
+
+    class_regex = re.compile(
+        r"class\s+"  # start with `class`
+        r"\w+\s+"  # class name
+        r"(((extends)|(implements))\s+\w+\s+)*"  # option inheritance
+        r"\{",
+        re.IGNORECASE & re.MULTILINE,
+    )
+
     start_line = 0
 
     # start at line "spot['line']" and work backwards until a line matches
-    for i in range(spot["line"], -1, -1):
-        line = lines[i]
-        if len(function_regex.findall(line)) > 0:
+    for i in range(start, -1, -1):
+        line = text[i]
+        if len(function_regex.findall(line)) + len(class_regex.findall(line)) > 0:
             start_line = i
             break
 
     # work our way down the function until we have closed all parenthesis
     rem_paren = 0
-    end_line = 0
-    for i in range(start_line, len(lines)):
-        line = lines[i]
+    end_line = start_line
+    for i in range(start_line, len(text)):
+        line = text[i]
         rem_paren += line.count("{")
         rem_paren -= line.count("}")
         if rem_paren == 0:
             end_line = i
             break
 
-    end_line = min(end_line, len(lines) - 1)
+    end_line = min(end_line, len(text) - 1)
 
-    print(f"Violating function at line(s) {start_line + 1}:{end_line + 1}  \\/")
+    return start_line, end_line
 
-    fun_text = "\n".join(lines[start_line:end_line + 1])
-    print(fun_text)
+spot = hotspots[-5]
 
-    return fun_text
+text, line, imp = parse_spot(spot)
 
-parse_spot(hotspots[0])
+exam = """
+Here is an example user message:
+
+The following java snippet contains the vulnerability `'password' detected in this expression, review this potentially hard-coded secret.` with a HIGH probability. 
+Fix the issue and format your code response as a markdown code block with the following format contained within `'''`:
+
+'''
+**Fixed Parent Section**
+```java
+...
+```
+
+**Imports**
+```java
+...
+```
+
+**Explanation**
+...
+'''
+The fixed parent section should not contain the described security vulnerability and should not change any function or class names that already exist in the vulnerable code.
+
+Here is the vulnerable code:
+Vulnerable line:
+```java
+String password = "Hunter1!";
+```
+
+Parent section of the vulnerable line:
+```java
+    public static void main(String[] args) throws IOException {
+        String password = "Hunter1!";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.print("Please enter password: ");
+        String user_guess = br.readLine();
+        
+        if (password.equals(user_guess))
+            System.out.println("\nCorrect!");
+        else
+            System.out.println("\nIncorrect!");
+    }
+```
+
+Current imports:
+```java
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+```
+
+---
+
+Here is he example system response:
+
+**Fixed Parent Section**
+```java
+public static void main(String[] args) throws IOException {
+    String password = System.getenv("PASSWORD");
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    System.out.print("Please enter password: ");
+    String user_guess = br.readLine();
+    
+    if (password.equals(user_guess))
+        System.out.println("\nCorrect!");
+    else
+        System.out.println("\nIncorrect!");
+}
+```
+
+**Imports**
+```java
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+```
+
+**Explanation**
+
+To fix the issue, you should be using an environment variable to set the password.
+This prevents a hard coded password from being extracted and allows for changing the password each run.
+"""
+"""
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Thing {
+    public static void main(String[] args) throws IOException {
+        String password = "Hunter1!";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.print("Please enter password: ");
+        String user_guess = br.readLine();
+        
+        if (password.equals(user_guess))
+            System.out.println("\nCorrect!");
+        else
+            System.out.println("\nIncorrect!");
+    }
+}
+"""
+
+prompt = (
+    f"The following java snippet contains the vulnerability "
+    f"`{spot['message']}` with a {spot['vulnerabilityProbability']} probability. "
+    f"Fix the issue and format your code response as a markdown code block"
+    f"with the following format contained within `'''`:"
+    f"\n'''\n**Fixed Parent Section**\n"
+    f"```java\n"
+    f"...\n"
+    f"```\n"
+    f"\n**Imports**\n"
+    f"```java\n"
+    f"...\n"
+    f"```\n"
+    f"\n**Explanation**\n"
+    f"...\n'''\n"
+    f"The fixed parent section should not contain the described security "
+    f"vulnerability and should not change any function or class names that "
+    f"already exist in the vulnerable code.\n"
+    f"\nHere is the vulnerable code:\n"
+    f"Vulnerable line:\n```java\n{line}\n```"
+    f"\n\nParent section of the vulnerable line:\n```java\n{text}\n```"
+    f"\n\nCurrent imports:\n```java\n{imp}\n```"
+)
+
+client = ollama.Client(
+    host="http://100.92.185.106:11434",
+)
+response = client.chat(
+    model="llama3.2",
+    messages=[
+        {
+            "role": "user",
+            "content": exam
+        },
+        {
+            "role": "user",
+            "content": prompt,
+        }
+    ]
+)
+
+print(f"TTR: {response.total_duration / 1e9}s:\n{response.message.content}")
