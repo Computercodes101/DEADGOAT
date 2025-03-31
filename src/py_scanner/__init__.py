7f22f74e37bde2ad5f50f1b4620aa41849426490
PilotLep <pilot1782@pilot1782.org>
2025-03-30 20:08:30 -0600

    splicing checkpoint

--- src/py_scanner/__init__.py
+++ src/py_scanner/__init__.py
@@ -102,10 +102,12 @@ def curly_context(text: list[str], start: int) -> tuple[int, int]:
         re.MULTILINE & re.IGNORECASE,
     )
 
+    decorator_regex = re.compile(r"\s*@(\w+\s*)(\([^)]*\))")
+
     class_regex = re.compile(
         r"class\s+"  # start with `class`
         r"\w+\s+"  # class name
-        r"(((extends)|(implements))\s+\w+\s+)*"  # option inheritance
+        r"(((extends)|(implements))\s+(\w+(<\w*>)?)\s+)*"  # option inheritance
         r"\{",
         re.IGNORECASE & re.MULTILINE,
     )
@@ -115,7 +117,11 @@ def curly_context(text: list[str], start: int) -> tuple[int, int]:
     # start at line "spot['line']" and work backwards until a line matches
     for i in range(start, -1, -1):
         line = text[i]
-        if len(function_regex.findall(line)) + len(class_regex.findall(line)) > 0:
+        if (
+                len(function_regex.findall(line)) +
+                len(class_regex.findall(line)) +
+                len(decorator_regex.findall(line))
+                > 0):
             start_line = i
             break
 
@@ -136,110 +142,6 @@ def curly_context(text: list[str], start: int) -> tuple[int, int]:
 
     return start_line, end_line
 
-
-exam = """
-Here is an example user message:
-
-The following java snippet contains the vulnerability `'password' detected in this expression, review this potentially hard-coded secret.` with a HIGH probability. 
-Fix the issue and format your code response as a markdown code block with the following format contained within `'''`:
-
-'''
-**Fixed Parent Section**
-```java
-...
-```
-
-**Imports**
-```java
-...
-```
-
-**Explanation**
-...
-'''
-The fixed parent section should not contain the described security vulnerability and should not change any function or class names that already exist in the vulnerable code.
-
-Here is the vulnerable code:
-Vulnerable line:
-```java
-String password = "Hunter1!";
-```
-
-Parent section of the vulnerable line:
-```java
-    public static void main(String[] args) throws IOException {
-        String password = "Hunter1!";
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        System.out.print("Please enter password: ");
-        String user_guess = br.readLine();
-        
-        if (password.equals(user_guess))
-            System.out.println("\nCorrect!");
-        else
-            System.out.println("\nIncorrect!");
-    }
-```
-
-Current imports:
-```java
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-```
-
----
-
-Here is he example system response:
-
-**Fixed Parent Section**
-```java
-public static void main(String[] args) throws IOException {
-    String password = System.getenv("PASSWORD");
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
-    System.out.print("Please enter password: ");
-    String user_guess = br.readLine();
-    
-    if (password.equals(user_guess))
-        System.out.println("\nCorrect!");
-    else
-        System.out.println("\nIncorrect!");
-}
-```
-
-**Imports**
-```java
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-```
-
-**Explanation**
-
-To fix the issue, you should be using an environment variable to set the password.
-This prevents a hard coded password from being extracted and allows for changing the password each run.
-"""
-"""
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-class Thing {
-    public static void main(String[] args) throws IOException {
-        String password = "Hunter1!";
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        System.out.print("Please enter password: ");
-        String user_guess = br.readLine();
-        
-        if (password.equals(user_guess))
-            System.out.println("\nCorrect!");
-        else
-            System.out.println("\nIncorrect!");
-    }
-}
-"""
 
 client = ollama.Client(
     host="https://pilot1782.org/desktop",
@@ -281,14 +183,11 @@ def get_fix(oclient: ollama.Client, hotspot: dict) -> tuple[str, str, str]:
         f"\n\nCurrent imports:\n```java\n{imp}\n```"
     )
 
+    response = None
     for _ in range(3):
         response = oclient.chat(
             model="qwen2.5-coder:14b",
             messages=[
-                {
-                    "role": "user",
-                    "content": exam
-                },
                 {
                     "role": "user",
                     "content": prompt,
@@ -300,7 +199,7 @@ def get_fix(oclient: ollama.Client, hotspot: dict) -> tuple[str, str, str]:
                 all([
                     i in response.message.content
                     for i in [
-                        "I'm sorry, but I can't help with that.",
+                        "I'm sorry, but I can't help with that",
                         "**Fixed Parent Section**",
                         "**Explanation**",
                         "**Imports**",
@@ -321,28 +220,25 @@ def get_fix(oclient: ollama.Client, hotspot: dict) -> tuple[str, str, str]:
     for line in response_text.splitlines():
         if "**Fixed Parent Section**" in line:
             in_section = True
-        if "```" in line and "java" not in line:
+        elif "```" in line and "java" not in line:
             in_section = False
-
-        if in_section:
+        elif in_section and "```" not in line and "java" not in line:
             fixed_parent_text += line + "\n"
 
     fixed_import_text = ""
     for line in response_text.splitlines():
         if "**Imports**" in line:
             in_section = True
-        if "```" in line and "java" not in line:
+        elif "```" in line and "java" not in line:
             in_section = False
-
-        if in_section:
+        elif in_section and "```" not in line and "java" not in line:
             fixed_import_text += line + "\n"
 
     explanation_text = ""
     for line in response_text.splitlines():
         if "**Explanation**" in line:
             in_section = True
-
-        if in_section:
+        elif in_section:
             explanation_text += line + "\n"
 
     if not fixed_parent_text or not fixed_import_text or not explanation_text:
@@ -355,15 +251,55 @@ def get_fix(oclient: ollama.Client, hotspot: dict) -> tuple[str, str, str]:
 
     return str(fixed_parent_text), str(fixed_import_text), str(explanation_text)
 
+def splice_fix(hotspot: dict, patch: str, imports: str) -> str:
+    """
+    Splices the patch into the file
+    :param hotspot: the hotspot to patch
+    :param patch: the patch to inject
+    :param imports: the imports to inject
+    :return: ...
+    """
+
+    file_path = hotspot["component"].split(":")[1]
+    file_path = os_ify_path(os.path.join(root_dir, file_path))
+    with open(file_path) as f:
+        text = f.read()
+
+    lines = text.split("\n")
+    start_line, end_line = curly_context(lines, hotspot["line"])
+
+    before_lines = lines[:start_line]
+    after_lines = lines[(end_line + 1):]
+    patched = patch.split("\n")
+    patched.insert(0, "// New code begins below")
+    patched.append("// New code ends here")
+
+    before_lines.extend(patched)
+    lines = before_lines
+    lines.extend(after_lines)
+
+    start_line = 9e999
+    for line in range(0, len(lines)):
+        if lines[line].startswith("import"):
+            if line < start_line:
+                start_line = line
+            end_line = line
+
+    before_lines = lines[:start_line]
+    after_lines = lines[(end_line + 1):]
+
+    return '\n'.join(before_lines) + '\n' + imports + '\n' + '\n'.join(after_lines)
+
 
 times = []
 for _ in range(1):
     spot = random.choice(hotspots)
 
     tStart = time.time()
-    _, _, explanation = get_fix(client, spot)
+    patch, imps, explanation = get_fix(client, spot)
     tDuration = time.time() - tStart
     print(f"Patch ({tDuration}s):\n{explanation}")
+    print(f"----\nFixed File:\n{splice_fix(spot, patch, imps)}")
     times.append(tDuration)
 
 print(f"\n\n----\nAverage response time: {sum(times) / len(times)}s, Min: {min(times)}, Max: {max(times)}")
